637e7c37b7f69b41eedb7a362bf091c837fc55a8
adam <athornton@lsst.org>
2025-01-28 18:11:16 -0700

    This should do it

--- differ.py
+++ differ.py
@@ -41,22 +41,46 @@ def main() -> None:
     exclude = check_for_conflicts(ghcr_shas, dockerhub_shas)
     print(f"Problematic tags: {exclude}")
     
-    ghcr_needs: dict[str, list[str]] = tag_compare(ghcr_tags, dockerhub_tags,
-                                                   exclude)
+    ghcr_needs: list[str] = tag_compare(ghcr_tags, dockerhub_tags, exclude)
 
     outfile=Path("ghcr-needs.json")
     outfile.write_text(json.dumps(ghcr_needs, sort_keys=True, indent=2))
+
+    outfile=Path("transfer-tags.sh")
+    out_text = "#!/bin/sh\n\n# Copy docker tags from Docker Hub to ghcr.io\n\n"
+    t=1
+    for tag in ghcr_needs:
+        out_text += f"# {t}\n"
+        out_text += f"docker pull docker.io/lsstsqre/sciplat-lab:{tag}\n"
+        out_text += (f"docker tag docker.io/lsstsqre/sciplat-lab:{tag} "
+                     f"ghcr.io/lsst-sqre/sciplat-lab:{tag}\n")
+        out_text += f"docker push ghcr.io/lsst-sqre/sciplat-lab:{tag}\n"
+        out_text += f"docker rmi -f ghcr.io/lsst-sqre/sciplat-lab:{tag}\n\n"
+        t+=1
+
+    for tag in exclude:
+        out_text += f"# {t}\n"
+        out_text += f"docker pull docker.io/lsstsqre/sciplat-lab:{tag}\n"
+        out_text += (f"docker tag docker.io/lsstsqre/sciplat-lab:{tag} "
+                     f"ghcr.io/lsst-sqre/sciplat-lab:exp_{tag}_docker_io\n")
+        out_text += f"docker push ghcr.io/lsst-sqre/sciplat-lab:exp_{tag}_docker_io\n"
+        out_text += f"docker rmi -f ghcr.io/lsst-sqre/sciplat-lab:exp_{tag}_docker_io\n\n"
+        t +=1
+        
+    out_text += "# That's all, folks.\n"
+
+    outfile.write_text(out_text)
+    
+        
 
 def tag_compare(ghcr: dict[str, list[str]], docker: dict[str, list[str]],
                 exclude: list[str]
                 ) -> dict[str, list[str]]:
     """Find tags in docker.io not at ghcr.io"""
-    retval: dict[str, list[str]] = {}
-
+    retval: list[str] = []
     for sha in docker:
         print(f"considering {docker[sha]}: {sha}")
         # I think we're going to have to do a pull/push anyway...
-        need_tags: list[str] = []
         for tag in docker[sha]:
             print(f"tag: {tag}")
             if ( tag and
@@ -66,18 +90,16 @@ def tag_compare(ghcr: dict[str, list[str]], docker: dict[str, list[str]],
                  not tag.startswith("recommended") ):
                 if sha not in ghcr:
                     print(f"sha {sha} not at ghcr")
-                    need_tags.append(tag)
+                    retval.append(tag)
                 else:
                     if tag not in ghcr[sha]:
                         print(f"tag {tag} not in in ghcr tags for {sha}")
-                        need_tags.append(tag)
+                        retval.append(tag)
                     else:
                         print(f"{tag} -> {sha} exists at both places")
             else:
                 print(f"Skipping tag '{tag}'")
-        if need_tags:
-            retval[sha] = [t for t in need_tags]
-                    
+
     return retval
 
 def check_for_conflicts(ghcr: dict[str, str],
@@ -91,16 +113,16 @@ def check_for_conflicts(ghcr: dict[str, str],
             print(
                 f"tag {tag} is {ghcr[tag]} at ghcr but {docker[tag]} at docker"
             )
-        if tag not in retval:
-            retval.append(tag)
+            if tag not in retval:
+                retval.append(tag)
     for tag in ghcr:
         sha = ghcr[tag]
         if tag in docker and ghcr[tag] != docker[tag]:
             print(
                 f"tag {tag} is {ghcr[tag]} at ghcr but {docker[tag]} at docker"
             )
-        if tag not in retval:
-            retval.append(tag)
+            if tag not in retval:
+                retval.append(tag)
     return retval
     
 if __name__ == "__main__":
